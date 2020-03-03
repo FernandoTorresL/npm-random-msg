eec5212c38723d6a62318a2f4b5b45e5e854821b
Fer Torres <fertorresmx@gmail.com>
2020-03-03 02:44:27 -0600

    Add .gitignore and first files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const messages = [
+	"Oscar",
+	"Diego",
+	"Laura",
+	"Demian",
+	"Ana"
+]
+
+const randomMsg = () => {
+	const message = messages[Math.floor(Math.random() *
+		messages.length)];
+		console.log(message);
+};
+
+module.exports = { randomMsg };
